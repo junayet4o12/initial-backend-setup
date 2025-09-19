cb1639e5ae4b584f6230b05438ba7a1a56ff9482
Junayet <muhammadjunayetmaruf@gmail.com>
2025-09-19 10:45:08 +0600

    asset module has added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { UserRouters } from '../modules/User/user.routes';
 import { MessageRouters } from '../modules/Messages/message.route';
 import { NotificationsRouters } from '../modules/Notification/notification.route';
 import { AuthRouters } from '../modules/Auth/auth.routes';
+import { AssetRouters } from '../modules/Asset/asset.route';
 const router = express.Router();
 
 const moduleRoutes = [
@@ -21,6 +22,10 @@ const moduleRoutes = [
   {
     path: '/notifications',
     route: NotificationsRouters,
+  },
+  {
+    path: '/assets',
+    route: AssetRouters,
   },
 ];
 
